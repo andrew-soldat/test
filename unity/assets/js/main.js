e4b2ab38a060a40fcac89acb57b22f96db25cad0
BlishchAndrew <blishch.andrew@gmail.com>
2021-05-06 10:05:02 +0300

    mobile

--- unity/assets/js/main.js
+++ unity/assets/js/main.js
@@ -1,4 +1,41 @@
-const imgItems = document.querySelectorAll('.main__bg');
+const imgItems = document.querySelectorAll('.main__bg'),
+   titlesFilter = document.querySelectorAll('.filter__title'),
+   body = document.querySelector('body'),
+   header = document.querySelector('header'),
+   burgerButton = document.querySelector('.header__burger'),
+   menuNav = document.querySelector('.header__nav');
+
+burgerButton.addEventListener('click', function (e) {
+   burgerButton.classList.toggle('active');
+   menuNav.classList.toggle('active');
+   body.classList.toggle('lock');
+});
+
+document.addEventListener('click', function (e) {
+   if (!e.target.closest('.header__nav, .header__burger')) {
+      burgerButton.classList.remove('active');
+		menuNav.classList.remove('active');
+		body.classList.remove('lock');
+		}
+});
+
+titlesFilter.forEach(item => {
+	item.addEventListener('click', function (){
+		item.classList.toggle('active');
+	})
+})
+
+body.addEventListener('click', function(e){
+	if (!e.target.closest('.filter__section')) {
+		titlesFilter.forEach(item => {
+			if (item.classList.contains('active')) {
+				item.classList.remove('active');
+			}
+		})
+	}
+})
+
+
 
 function parallax(e) {
    imgItems.forEach((item) => {
@@ -41,6 +78,54 @@ $(window).scroll(function () {
 });
 $(document).ready(function(){
 
+	$('.partners').slick({
+		infinite: true,
+		slidesToShow: 5,
+		slidesToScroll: 1,
+		dots: false,
+		responsive: [
+			{
+				breakpoint: 1250,
+				settings: {
+				  arrows: false,
+				  dots: true
+				}
+			},
+			{
+				breakpoint: 991,
+				settings: {
+					slidesToShow: 4,
+						arrows: false,
+						dots: true
+				}
+			},
+			{
+				breakpoint: 765,
+				settings: {
+					slidesToShow: 3,
+						arrows: false,
+						dots: true
+				}
+			},
+			{
+				breakpoint: 565,
+				settings: {
+					slidesToShow: 2,
+						arrows: false,
+						dots: true
+				}
+			},
+			{
+				breakpoint: 450,
+				settings: {
+					slidesToShow: 1,
+						arrows: false,
+						dots: true
+				}
+			},
+		]
+	});
+
 	$('.slider-last-new__fullscreen').slick({
 		slidesToShow: 1,
 		slidesToScroll: 1,
@@ -61,11 +146,23 @@ $(document).ready(function(){
 	$('.slider-events').slick({
 		arrows: false,
 		dots: true,
-		responsive: []
+		adaptiveHeight: true,
+		responsive: [
+			
+		]
 	});
 
 	$('.news__slider').slick({
 		infinite: false,
-		responsive: []
+		adaptiveHeight: true,
+		responsive: [
+			{
+				breakpoint: 1250,
+				settings: {
+				  arrows: false,
+				  dots: true
+				}
+			},
+		]
 	});
 });
